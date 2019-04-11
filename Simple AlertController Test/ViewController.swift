57ce7b8558a1889c5800d84914a5568cbed55ff6
Jonghyun Kim <jhkim@dit.ac.kr>
2019-04-11 10:54:36 +0900

    Initial Commit

---
+++ Simple AlertController Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simple AlertController Test
+//
+//  Created by amadeus on 11/04/2019.
+//  Copyright © 2019 DIT Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
